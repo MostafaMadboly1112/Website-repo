4ce238ff3a0ea782c211a48c6c4a140c80317660
MostafaMadboly1112 <70201722+MostafaMadboly1112@users.noreply.github.com>
2024-03-07 18:09:23 +0200

    updating

--- booking_tool/api/models.py
+++ booking_tool/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Ticket(models.Model):
+    avs = models.CharField(max_length=16, default="", unique=True)
+    user = models.CharField(max_length=30, unique=True)
+    booking_from = models.DateTimeField()
+    booking_to = models.DateTimeField()
+    Tec_data = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
