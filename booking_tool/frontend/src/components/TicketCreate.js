fbeff1fcc8a8ad9cff0d98536b355f471cdd06c5
MostafaMadboly1112 <70201722+MostafaMadboly1112@users.noreply.github.com>
2024-03-08 22:52:06 +0200

    update for dateTime picker

--- booking_tool/frontend/src/components/TicketCreate.js
+++ booking_tool/frontend/src/components/TicketCreate.js
@@ -4,11 +4,16 @@ import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import { Link } from "react-router-dom";
 import Container from "@material-ui/core/Container";
-import { makeStyles } from "@material-ui/core";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 
 export default function TicketCreate() {
   const btn = { backgroundColor: "lightblue", fontSize: 20 };
   const field = { marginTop: 20, marginBottom: 20, display: "block" };
+  const [value, setValue] = useState(null);
+  console.log(value);
   const [avs, setAVS] = useState("");
   const [user, setUSER] = useState("");
   const [tec, setTEC] = useState("");
@@ -30,7 +35,7 @@ export default function TicketCreate() {
       setTECError(true);
     }
 
-    if (avs && user && tec) {
+    if (avs && user && tec && from) {
       console.log(avs, user, tec);
     }
   };
@@ -60,6 +65,21 @@ export default function TicketCreate() {
           required
           error={userError}
         />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer components={["DateTimePicker"]}>
+            <DateTimePicker
+              label="Booking from"
+              value={value}
+              onChange={(newValue) => setValue(newValue)}
+              slotProps={{ textField: { variant: "outlined" } }}
+            />
+          </DemoContainer>
+        </LocalizationProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer components={["DateTimePicker"]}>
+            <DateTimePicker label="Booking to" />
+          </DemoContainer>
+        </LocalizationProvider>
         <TextField
           onChange={(e) => setTEC(e.target.value)}
           style={field}
