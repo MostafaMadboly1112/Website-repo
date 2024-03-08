8d9e3ad14d92e121dd73b801915811b90e352af5
MostafaMadboly1112 <70201722+MostafaMadboly1112@users.noreply.github.com>
2024-03-08 19:47:34 +0200

    updating create page

--- booking_tool/frontend/src/components/TicketCreate.js
+++ booking_tool/frontend/src/components/TicketCreate.js
@@ -1,10 +1,48 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import { Link } from "react-router-dom";
+import Container from "@material-ui/core/Container";
 
-export default class TicketCreate extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <p>This is the TicketCreate</p>;
-  }
+export default function TicketCreate() {
+  return (
+    <Container>
+      <Typography variant="h2" color="primary" align="center" gutterBottom>
+        Create A Ticket
+      </Typography>
+      <form noValidate autoComplete="off">
+        <TextField
+          label="AVS"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+        />
+        <TextField
+          label="User"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+        />
+        <TextField
+          label="Techniker Data"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+        />
+      </form>
+
+      <Button
+        onClick={() => console.log("You Clicked me")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
